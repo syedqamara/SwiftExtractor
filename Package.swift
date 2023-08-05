cc0464f6b283e18ec9c9e977efbb8a9a5a6b8dac
Syed Qamar Abbas <syedqamar.a1@gmail.com>
2023-08-06 02:24:52 +0500

    Fixes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "508.0.0"),
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/syedqamara/core_architecture.git", from: "1.0.2"),
+         .package(url: "https://github.com/syedqamara/core_architecture.git", from: "1.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
